7e5b6ef2655e46361b442bcf993beb9cd935d3c0
Mr-jalil <idazziabdeljalil@gmail.com>
2023-03-18 05:01:18 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,20 +6,12 @@
  */
 int main(void)
 {
-int i, j;
-for (i = 0; i < 10; i++)
-{
-for (j = i + 1; j < 10; j++)
-{
-putchar(i + '0');
-putchar(j + '0');
-if (i != 8 || j != 9)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
 putchar('\n');
-return (0);
+return(0);
 }
+
+
+
