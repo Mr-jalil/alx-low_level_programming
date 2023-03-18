84d6444a2baf62434dda5288952e0c6f599a7830
Mr-jalil <idazziabdeljalil@gmail.com>
2023-03-18 05:07:29 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,16 +2,16 @@
 /**
  * main - prints all possible different combinations of two digits.
  *
- * Return: Always 0 (Success)
+ * Return: Always (Success)
  */
+
 int main(void)
+
 {
 char c;
+
 for (c = 'a'; c <= 'z'; c++)
 putchar(c);
 putchar('\n');
 return(0);
 }
-
-
-
