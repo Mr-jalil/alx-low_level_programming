4c4ae053308d30cb456655a48b80e869d2ed8f6d
Mr-jalil <idazziabdeljalil@gmail.com>
2023-03-21 09:02:25 -0700

    main.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,13 @@
-#include <stdio.h>
-
-/**
- *
- * main - prints _putchar, followed by a new line.
- *
- * return: 0
- *
- */
-
-int main (void)
-{
-putchar('a' );
-putchar('b' );
-putchar('\n');
-return(0);
-}
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
