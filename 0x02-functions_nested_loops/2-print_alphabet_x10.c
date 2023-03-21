a559456fc4f6e466ea334da1311b7bf16fe38ae6
Mr-jalil <idazziabdeljalil@gmail.com>
2023-03-21 10:02:54 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main  prints 10 times the alphabet, in lowercase
+ * return:0
+ */
+void print_alphabet_x10(void);
+char = i;
+     while(  i = 1;)
+
+{
+	_putchar(i=1 , i <= 10 , i++);
+}
+{
+	_putchar('\n');
+}
