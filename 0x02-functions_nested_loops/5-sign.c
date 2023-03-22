11ec05a08cf624f3fb50ddb121d74ed6bd687961
Mr-jalil <idazziabdeljalil@gmail.com>
2023-03-22 06:39:01 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,21 +1,26 @@
 #include "main.h"
-/**
- * main - prints the sign of a numbe
- * @c : c is a letter, lowercase or uppercase
- * return:1 if n is greater than zero , 0 if n is zero, -1 if n is less than zero
- */
- int print_sign(int n);
-{
-	if( n > 0 , n >= 1 , n++ );
-	putchar(+),
 
-{     
-       	else if 
-}
-     ( n = 0 , n <= 0 , --n );
-     putchar (0);
-{ 
-      else 
-}
-	putchar(-);
+/**
+ * print_sign - print sign of number
+ * @n: number to check
+ * Return: 1 if greater than 0, 0 if 0, -1 if less than 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
 }
