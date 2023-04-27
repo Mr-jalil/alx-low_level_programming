4313ca55f31d1417cec3fdcfb9a73b848614752c
Mr-jalil <idazziabdeljalil@gmail.com>
2023-04-27 10:52:47 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
- * print_list - function that prints all the elements
- * return: the number of nodes
- * @h: link list
+ * print_list - a function that prints all the elements of a list_t list.
+ * @h: linked list
+ * Return: number of nodes in linked list
  */
 
 size_t print_list(const list_t *h)
-	{
+{
 	size_t count = 0;
 
 	while (h != NULL)
