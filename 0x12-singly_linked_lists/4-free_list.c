24e85130d60397c3366b50ffe3049c9dcc7bb927
Mr-jalil <idazziabdeljalil@gmail.com>
2023-04-28 05:10:55 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - function that frees linked list
+ * @head: list_t list to be free
+ */
+
+void free_list(list_t *head)
+{
+list_t *jalil;
+while(head)
+{
+jalil = head->next;
+free(head->str);
+free(head);
+head = jalil;
+}
+}
