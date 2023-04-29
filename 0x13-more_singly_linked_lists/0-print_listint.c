a648cef9a53d70fefee787602e2d4fb1a031273c
Mr-jalil <idazziabdeljalil@gmail.com>
2023-04-29 04:49:00 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t mynum  = 0;
+    size_t number = 0;
 
     while (h)
     {
         printf("%d\n", h->n);
-        mynum++;
+        number++;
         h = h->next;
     }
 
-    return (mynum);
+    return (number);
 }
